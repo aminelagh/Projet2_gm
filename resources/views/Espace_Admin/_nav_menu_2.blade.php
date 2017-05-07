b764b0cfa4befbbe9645d19c63e145b9dcc91e38
aminelagh <amine.laghlabi@gmail.com>
2017-05-08 00:50:22 +0100

    .

--- resources/views/Espace_Admin/_nav_menu_2.blade.php
+++ resources/views/Espace_Admin/_nav_menu_2.blade.php
@@ -3,10 +3,9 @@
 
     <li><a href="{{ Route('admin.home') }}"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a></li>
 
-    <li><a href="{{ Route('admin.add',['p_table'=>'users']) }}"><i class="fa fa-fw fa-desktop"></i> Ajouter User</a></li>
+    <li><a href="{{ Route('admin.add',['p_table'=>'users']) }}"><i class="fa fa-fw fa-plus"></i> Ajouter User</a></li>
 
-    <li><a href="{{ Route('admin.lister',['p_table'=>'users']) }}"><i class="fa fa-fw fa-wrench"></i> Liste User <span class="badge">{{ App\Models\User::count() }} </span> </a></li>
-
+    <li><a href="{{ Route('admin.lister',['p_table'=>'users']) }}"><i class="fa fa-fw fa-desktop"></i> Liste User <span class="badge">{{ App\Models\User::count() }} </span> </a></li>
 
   </ul>
 </div>
